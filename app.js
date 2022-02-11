d980cf307a4f9718dc6ca130ceeea8a1bcacefee
Abygaelle <abygaelleb02@gmail.com>
2022-02-11 14:11:46 -0500

    finish stubbing

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-
+const winningArrays = []
+console.log(winningArrays)
 /*-------------------------------- Variables --------------------------------*/
-
+let plaerTurn, message, winner, circles
 
 
 /*------------------------ Cached Element References ------------------------*/
-console.log('sanity check')
+const resetBtn = document.querySelector('#reset-button')
+const gridCicrles = document.querySelectorAll('.cirlce')
+const messages = document.querySelector('#message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+resetBtn.addEventListener('click', init)
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+function init() {
+
+}
+
+function render() {
+
+}
